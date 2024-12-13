184c6c944df2e5938699c94e1552ecb10f44c2b2
Alexanderdunlop <alexjamesdunlop@gmail.com>
2024-12-13 22:11:25 +0000

    Replace TavilySearchResults with get_now function to return current time

--- my_agent/utils/tools.py
+++ my_agent/utils/tools.py
@@ -1,3 +1,18 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
+# from langchain_community.tools.tavily_search import TavilySearchResults
 
-tools = [TavilySearchResults(max_results=1)]
+# tools = [TavilySearchResults(max_results=1)]
+
+from datetime import datetime
+from typing import Annotated, TypedDict, Union
+
+from langchain_core.tools import tool
+
+@tool
+def get_now(format: str = "%Y-%m-%d %H:%M:%S"):
+    """
+    Get the current time
+    """
+    return datetime.now().strftime(format)
+
+
+tools = [get_now]
