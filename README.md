0f18e0274cd58ebcd38bae427e0524a3bfbb6d60
Alexanderdunlop <alexjamesdunlop@gmail.com>
2024-12-13 21:17:35 +0000

    Add initial project structure with README, .gitignore, and environment configuration

---
+++ README.md
@@ -0,0 +1,2 @@
+# Memory GPT Example
+
---
+++ my_agent/utils/nodes.py
@@ -0,0 +1,39 @@
+from functools import lru_cache
+from langchain_anthropic import ChatAnthropic
+from langchain_openai import ChatOpenAI
+from my_agent.utils.tools import tools
+from langgraph.prebuilt import ToolNode
+
+
+@lru_cache(maxsize=4)
+def _get_model(model_name: str):
+    if model_name == "openai":
+        model = ChatOpenAI(temperature=0, model_name="gpt-4o")
+    elif model_name == "anthropic":
+        model =  ChatAnthropic(temperature=0, model_name="claude-3-sonnet-20240229")
+    else:
+        raise ValueError(f"Unsupported model type: {model_name}")
+
+    model = model.bind_tools(tools)
+    return model
+
+def should_continue(state):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    else:
+        return "continue"
+
+
+system_prompt = """Be a helpful assistant"""
+
+def call_model(state, config):
+    messages = state["messages"]
+    messages = [{"role": "system", "content": system_prompt}] + messages
+    model_name = config.get('configurable', {}).get("model_name", "anthropic")
+    model = _get_model(model_name)
+    response = model.invoke(messages)
+    return {"messages": [response]}
+
+tool_node = ToolNode(tools)
